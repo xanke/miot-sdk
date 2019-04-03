bf4f93997c3471b53e6b284d172b21549b5bf992
fanbo <xank@qq.com>
2019-04-04 00:30:50 +0800

    icon

--- projects/edc.bhf_light.bhf01/components/MButton.js
+++ projects/edc.bhf_light.bhf01/components/MButton.js
@@ -1,24 +1,25 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Image } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
     width: 100,
     borderWidth: 1,
-    borderColor: '#CCC',
+    borderColor: '#eee',
     borderRadius: 3,
+    marginLeft: 4,
     padding: 12
   },
   icon: {
     width: 30,
     height: 30,
-    textAlign: 'center',
-    backgroundColor: '#999999',
+    marginLeft: 'auto',
+    marginRight: 'auto',
   },
   text: {
     textAlign: 'center',
     marginTop: 12,
-    fontSize: 18
+    fontSize: 13
   }
 })
 
@@ -30,11 +31,16 @@ export class MButton extends React.Component {
       status: 0,
     }
   }
-
+  
   render() {
+    const { icon = 'light' } = this.props
+    let iconImg
+    if (icon === 'light' ) iconImg = require('../assets/icon/light.png')
+    if (icon === 'power' ) iconImg = require('../assets/icon/power.png')
+    if (icon === 'fan' ) iconImg = require('../assets/icon/fan.png')
     return (
       <View style={styles.container}>
-        <Text style={styles.icon}></Text>
+        <Image style={styles.icon} source={iconImg}></Image>
         <Text style={styles.text}>{this.props.name}</Text>
       </View>
     )
--- projects/edc.bhf_light.bhf01/index.js
+++ projects/edc.bhf_light.bhf01/index.js
@@ -5,6 +5,13 @@ import { View, Text, Button, StyleSheet } from 'react-native'
 import miotui from 'miot/ui'
 import { MessageDialog } from 'miot/ui'
 import { MButton } from './components/MButton'
+import {
+  RkButton,
+  RkText,
+  RkCard,
+  RkSwitch,
+  RkTheme,
+} from 'react-native-ui-kitten';
 
 class App extends React.Component {
   constructor() {
@@ -12,7 +19,8 @@ class App extends React.Component {
     this.state = {
       action: '3232',
       sendData: '',
-      data: ''
+      data: '',
+      value: false
     }
     this.fetchSet('set_light_switch', {params: [true]})
   }
@@ -21,6 +29,10 @@ class App extends React.Component {
     params.id = 1
     this.fetchData(method, params)
   }
+
+  onSwitchValueChange = (value) => {
+    this.setState({value: value});
+  };
 
   fetchData(method, params) {
     // const params = { id: 1, method: 'props', params: { power_switch: true } }
@@ -52,29 +64,37 @@ class App extends React.Component {
             navigation.goBack()
           }}
         />
+        <RkButton rkType='clear link'>
+          <RkText rkType='accent'>18 Likes</RkText>
+        </RkButton>
+        <RkSwitch
+          value={this.state.value}
+          onValueChange={this.onSwitchValueChange}
+        />
+
         <Text>灯光控制</Text>
         <View style={styles.buttonContainer}>
-          <MButton onPress={() => this.fetchSet('set_light_switch', {params: [true]})} name='灯光开' />
-          <MButton onPress={() => this.fetchSet('set_light_switch', {params: [false]})} name='灯光关' />
+          <MButton icon="light" onPress={() => this.fetchSet('set_light_switch', {params: [true]})} name='灯光开' />
+          <MButton icon="light" onPress={() => this.fetchSet('set_light_switch', {params: [false]})} name='灯光关' />
         </View>
         <Text>电源控制</Text>
         <View style={styles.buttonContainer}>
-          <MButton onPress={() => this.fetchSet('set_power_switch ', {params: [true]})} name='电源开' />
-          <MButton onPress={() => this.fetchSet('set_power_switch ', {params: [false]})} name='电源关' />
+          <MButton icon="power" onPress={() => this.fetchSet('set_power_switch ', {params: [true]})} name='电源开' />
+          <MButton icon="power" onPress={() => this.fetchSet('set_power_switch ', {params: [false]})} name='电源关' />
         </View>
         <Text>风速控制</Text>
         <View style={styles.buttonContainer}>
-          <MButton onPress={() => this.fetchSet('set_wind_speed ', {params: [0]})} name='关' />
-          <MButton onPress={() => this.fetchSet('set_wind_speed ', {params: [1]})} name='风速1' />
-          <MButton onPress={() => this.fetchSet('set_wind_speed ', {params: [2]})} name='风速2' />
-          <MButton onPress={() => this.fetchSet('set_wind_speed ', {params: [3]})} name='风速3' />
+          <MButton icon="fan" onPress={() => this.fetchSet('set_wind_speed ', {params: [0]})} name='关' />
+          <MButton icon="fan" onPress={() => this.fetchSet('set_wind_speed ', {params: [1]})} name='风速1' />
+          <MButton icon="fan" onPress={() => this.fetchSet('set_wind_speed ', {params: [2]})} name='风速2' />
+          <MButton icon="fan" onPress={() => this.fetchSet('set_wind_speed ', {params: [3]})} name='风速3' />
         </View>
         <Text>工作模式</Text>
         <View style={styles.buttonContainer}>
-          <MButton onPress={() => this.fetchSet('set_work_mode ', {params: [0]})} name='模式0' />
-          <MButton onPress={() => this.fetchSet('set_work_mode ', {params: [1]})} name='模式1' />
-          <MButton onPress={() => this.fetchSet('set_work_mode ', {params: [2]})} name='模式2' />
-          <MButton onPress={() => this.fetchSet('set_work_mode ', {params: [3]})} name='模式3' />
+          <MButton icon="light" onPress={() => this.fetchSet('set_work_mode ', {params: [0]})} name='模式0' />
+          <MButton icon="light" onPress={() => this.fetchSet('set_work_mode ', {params: [1]})} name='模式1' />
+          <MButton icon="light" onPress={() => this.fetchSet('set_work_mode ', {params: [2]})} name='模式2' />
+          <MButton icon="light" onPress={() => this.fetchSet('set_work_mode ', {params: [3]})} name='模式3' />
         </View>
         <Text>{this.state.sendData}</Text>
         <Text>{this.state.data}</Text>
